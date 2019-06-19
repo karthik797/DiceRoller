038c52fe4303fabb61b0296d4f98888775bf19af
vodafone <vodafone@vodafones-MacBook-Pro-2.local>
2019-06-19 15:05:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.text="Let's Role"
+    }
+}
